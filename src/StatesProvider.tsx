8462507db63d22de83b4ef1b12a222b1e7f69b88
AdenPun <35649969+aappaapp@users.noreply.github.com>
2023-01-28 01:42:12 +0800

    Deleted unuse comments

--- src/StatesProvider.tsx
+++ src/StatesProvider.tsx
@@ -21,7 +21,6 @@ export const StatesProvider: React.FC<StoreProviderProps> = (props) => {
     const defaultStates: InternalStateCollection = { ...parentContext, ...props.states } as any;
 
     const [states, setStates] = React.useState(() => {
-        // push  =  ()->{}
         setProp = (state, prop, value) => {
             setStates((originalState) => {
                 if (isFunction(value)) value = value(originalState[state]);
@@ -50,33 +49,6 @@ export const StatesProvider: React.FC<StoreProviderProps> = (props) => {
             setValue(state, states[state].initialValue);
         });
     }, []);
-
-    // React.useMemo(() => {
-    //     setValue = (state, value) => {
-    //         if (!defaultStates[state].set) {
-    //             defaultStates[state].set = (value: SetterArg<any>) => {
-    //                 const isFunction = (v: any): v is Function => {
-    //                     if (typeof v === "function") return true;
-    //                     return false;
-    //                 };
-    //                 if (isFunction(value)) value = value(defaultStates[state].value);
-    //                 defaultStates[state].value = value;
-    //                 console.log(defaultStates[state].value);
-    //                 defaultStates[state].onChange?.(value);
-    //                 defaultStates[state].subs.forEach((sub) => {
-    //                     sub(defaultStates[state]);
-    //                 });
-    //                 return;
-    //             };
-    //         }
-    //         defaultStates[state].set(value);
-    //     };
-    //     Object.keys(defaultStates).forEach((state) => {
-    //         console.log(Object.keys(defaultStates));
-    //         defaultStates[state].subs = [];
-    //         setValue(state, defaultStates[state].initialValue);
-    //     });
-    // }, []);
 
     return <StoreContext.Provider value={states}>{props.children}</StoreContext.Provider>;
 };
